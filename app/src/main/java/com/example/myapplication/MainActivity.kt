1814f4582456db1708a2d97a254f6cd6d443aeeb
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-04-14 23:45:08 -0300

    feat: Adicionei tela de cadastro de nome

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nameInputText: TextInputEditText
+    private lateinit var btnNext: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        nameInputText = findViewById(R.id.textName)
+        btnNext = findViewById(R.id.btnNext)
+
+        btnNext.setOnClickListener {
+            showUserName()
+        }
+    }
+
+    private fun showUserName() {
+        val name = nameInputText.text.toString().trim()
+        if (name.isEmpty()) {
+            Toast.makeText(this, "Você não digitou seu nome", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Olá, $name!", Toast.LENGTH_SHORT).show()
         }
     }
 }
