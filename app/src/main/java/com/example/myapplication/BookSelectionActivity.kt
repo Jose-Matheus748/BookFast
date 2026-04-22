cfb8c05464a8e7d012d6d426f4398ad2f4118afd
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-04-22 04:04:28 -0300

    feat: terminando de estruturar a página de seleção de livros

--- app/src/main/java/com/example/myapplication/BookSelectionActivity.kt
+++ app/src/main/java/com/example/myapplication/BookSelectionActivity.kt
@@ -1,12 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class BookSelection : AppCompatActivity() {
+class BookSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_selection)
