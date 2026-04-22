ad5bbd3edcdbd73050d7bb78f1fc4ee57de430c6
Jose-Matheus748 <josemathaguiar@gmail.com>
2026-04-22 00:37:33 -0300

    Feat: Adicionando Header configurado para usuario admin e tela sobre o aplicativo

--- app/src/main/java/com/example/myapplication/AboutActivity.kt
+++ app/src/main/java/com/example/myapplication/AboutActivity.kt
@@ -1,21 +1,16 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class BookpageActivity : AppCompatActivity() {
-    lateinit var capaCatedral: ImageView
-
+class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bookpage)
-
-        capaCatedral = findViewById(R.id.capaFortaleza)
+        setContentView(R.layout.activity_about)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.AboutPage)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
