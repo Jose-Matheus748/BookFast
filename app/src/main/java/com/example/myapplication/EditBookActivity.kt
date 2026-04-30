c3ca47bc85af05ab366b1051c797c74b4c1dd26a
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-04-30 01:50:02 -0300

    feat: subindo minha tela da semana3 - EditBookActivity - e melhorias na tela BookpageActivity

--- app/src/main/java/com/example/myapplication/EditBookActivity.kt
+++ app/src/main/java/com/example/myapplication/EditBookActivity.kt
@@ -1,20 +1,16 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class BookpageActivity : AppCompatActivity() {
-    lateinit var capaCatedral: ImageView
-
+class EditBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bookpage)
-
-        capaCatedral = findViewById(R.id.capaFortaleza)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.AboutPage)) { v, insets ->
+        setContentView(R.layout.activity_edit_book)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
