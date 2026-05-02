e4c379a4e5f7c0b5badcaf25fd0416d69dd60f4d
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-05-01 21:12:10 -0300

    feat: fiz a navegação entre a tela inicial -> activity_wellcome

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
+    lateinit var btnNavegar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicial)
+
+        val btnNavegar = findViewById<Button>(R.id.btnNavegar)
+
+        // activity_tela_inicial -> activity_wellcome
+        btnNavegar.setOnClickListener {
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
