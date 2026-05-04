04ef85100a3733bb0692b112fc8f38b9bc568125
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-05-03 22:40:10 -0300

    retirando dependencias desnecessarias

--- app/src/main/java/com/example/myapplication/CreateBookActivity.kt
+++ app/src/main/java/com/example/myapplication/CreateBookActivity.kt
@@ -3,10 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class CreateBookActivity : AppCompatActivity() {
 
