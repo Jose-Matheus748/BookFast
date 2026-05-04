abaec88b87352a68c46c7b91d708da98e7807778
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-05-04 02:03:51 -0300

    feat: terminei a Página de detalhes do livro na visão do admin, e a página de editar livro

--- app/src/main/java/com/example/myapplication/AdminEditBookActivity.kt
+++ app/src/main/java/com/example/myapplication/AdminEditBookActivity.kt
@@ -3,9 +3,9 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class AdminBookpageReference : AppCompatActivity() {
+class AdminEditBookActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_bookpage_reference)
+        setContentView(R.layout.activity_admin_edit_book)
     }
 }
