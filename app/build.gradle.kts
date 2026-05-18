192b596762587693109834b34a3966231e49220c
Jose-Matheus748 <josemathaguiar@gmail.com>
2026-05-17 23:14:30 -0300

    Realizando integração com o banco de dados no firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,4 +48,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.firebase:firebase-firestore-ktx:25.1.4")
 }
