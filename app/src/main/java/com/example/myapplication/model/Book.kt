b65e84ba1812b8aeb1407daf53db59fc4e19f8c9
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-05-19 05:44:13 -0300

    feat: adicionando atributo de capaBase64 no model de Book para puxar do firestore

--- app/src/main/java/com/example/myapplication/model/Book.kt
+++ app/src/main/java/com/example/myapplication/model/Book.kt
@@ -3,5 +3,6 @@ package com.example.myapplication.model
 data class Book(
     val title: String,
     val author: String,
-    val imageUrl: Int
+    val imageUrl: Int, // Guarda uma imagem local padrão, usada se não tiver capa real.
+    val capaBase64: String? = null // Guarda a capa real vinda do Firestore em Base64; pode ser nula.
 )
