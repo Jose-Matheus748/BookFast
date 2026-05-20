7196797a4ff357b371e51a91025e7e0f42f7d4b2
Chas3xW4yne <chase.w4yne@gmail.com>
2026-05-20 13:37:04 -0300

    fiz umas mudanças em partes do marcador de usuário e testei a funcionalidade de login

--- app/src/main/java/com/example/myapplication/User.kt
+++ app/src/main/java/com/example/myapplication/User.kt
@@ -4,5 +4,5 @@ data class User(
     val uid: String = "",
     val nome: String = "",
     val email: String = "",
-    val perfil: String = "cliente" // ou "profissional"
+    val perfil: String = "",
 )
