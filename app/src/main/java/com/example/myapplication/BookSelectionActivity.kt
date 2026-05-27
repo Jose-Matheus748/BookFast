4ab11336123ab02b0ea3bd280cb2c0073d7511ff
SimonSousa25 <simonoliveirasousa02@gmail.com>
2026-05-26 21:53:21 -0300

    feat: integrando BookpageActivity e BookSelectionActivity ao id dos livros no firestore

--- app/src/main/java/com/example/myapplication/BookSelectionActivity.kt
+++ app/src/main/java/com/example/myapplication/BookSelectionActivity.kt
@@ -1,14 +1,40 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.ScrollView
-import android.content.Intent
+import android.widget.TextView
+import android.widget.Toast
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.adapter.BookAdapter
+import com.example.myapplication.model.Book
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class BookSelectionActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var painelSelecionados: ScrollView
+    private lateinit var painelReservas: ScrollView
+    private lateinit var recyclerLivrosSelecionados: RecyclerView
+    private lateinit var adapter: BookAdapter
+
+    // IDs dos livros selecionados na activity
+    private lateinit var tituloLivroSelecionado: TextView
+    private lateinit var autoresLivroSelecionado: TextView
+    private lateinit var capaLivroSelecionado: ImageView
+
+    private val db = Firebase.firestore
+
+    private val livrosDoFirestore = mutableListOf<Book>()
 
     lateinit var imgFortaleza300 : ImageView
     lateinit var imgFortaleza300Alugado : ImageView
@@ -17,41 +43,121 @@ class BookSelectionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_selection)
 
+        iniciarViews()
+        prepararRecyclerView()
+        configurarAbas()
+        carregarLivroSelecionado()
+
         HeaderNavigation.setup(this)
         FooterNavigation.setup(this)
+    }
 
-        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
-        val painelSelecionados = findViewById<ScrollView>(R.id.painelSelecionados)
-        val painelReservas = findViewById<ScrollView>(R.id.painelReservas)
+    private fun iniciarViews() {
+        tabLayout = findViewById(R.id.tabLayout)
+        painelSelecionados = findViewById(R.id.painelSelecionados)
+        painelReservas = findViewById(R.id.painelReservas)
+        recyclerLivrosSelecionados = findViewById(R.id.recyclerLivrosSelecionados)
+
+        tituloLivroSelecionado = findViewById(R.id.tituloLivro)
+        autoresLivroSelecionado = findViewById(R.id.autoresLivro)
+        capaLivroSelecionado = findViewById(R.id.capaFortaleza)
 
         imgFortaleza300 = findViewById(R.id.capaFortaleza)
         imgFortaleza300Alugado = findViewById(R.id.capaFortalezaAlugado)
+    }
 
-        imgFortaleza300.setOnClickListener {
+    private fun prepararRecyclerView() {
+        adapter = BookAdapter(livrosDoFirestore) { livro ->
             val intent = Intent(this, BookpageActivity::class.java)
+            intent.putExtra("LIVRO_ID", livro.id) // envia o ID do livro para a BookpageActivity; esse ID vem do firestore
             startActivity(intent)
         }
 
-        imgFortaleza300Alugado.setOnClickListener {
-            val intent = Intent(this, BookpageActivity::class.java)
-            startActivity(intent)
+        recyclerLivrosSelecionados.apply {
+            layoutManager = GridLayoutManager(this@BookSelectionActivity, 2)
+            adapter = this@BookSelectionActivity.adapter
         }
+    }
 
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+    private fun configurarAbas() {
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 when (tab.position) {
-                    0 -> {
+                    0 -> { // aba de selecionados
                         painelSelecionados.visibility = View.VISIBLE
                         painelReservas.visibility     = View.GONE
                     }
-                    1 -> {
+
+                    1 -> { // aba de reservados
                         painelSelecionados.visibility = View.GONE
                         painelReservas.visibility     = View.VISIBLE
                     }
                 }
             }
+
             override fun onTabUnselected(tab: TabLayout.Tab) {}
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
+    }
+
+    private fun carregarIdDoLivroSelecionado(): String {
+        val livroId = intent.getStringExtra("LIVRO_ID") ?: ""
+
+        if (livroId.isEmpty()) {
+            Toast.makeText(
+                this,
+                "Livro não encontrado",
+                Toast.LENGTH_SHORT)
+                .show()
+            finish()
+            return "Id do livro não carregado; livroId: $livroId"
+        }
+
+        Log.d("BookFast", "ID do livro recebido: $livroId")
+
+        return livroId
+    }
+
+    private fun carregarLivroSelecionado() {
+        val livroSelecionadoId = carregarIdDoLivroSelecionado()
+
+        db.collection("Livros")
+            .document(livroSelecionadoId) // pega APENAS o livro clicado
+            .get()
+            .addOnSuccessListener { documentoLivro ->
+                val titulo = documentoLivro.getString("titulo") ?: "Sem título"
+
+                val autores = (documentoLivro.get("autores") as? List<*>)
+                    ?.filterIsInstance<String>()
+                    ?.joinToString(", ")
+                    ?: "Autor não informado"
+
+                val capaBase64 = documentoLivro.getString("capaUrl") ?: ""
+
+                carregarCapaDoLivro(capaBase64)
+
+                tituloLivroSelecionado.text = titulo
+                autoresLivroSelecionado.text = autores
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Erro ao carregar livro selecionado", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+    private fun carregarCapaDoLivro(capaDoLivro: String) {
+        if (capaDoLivro.isNotEmpty()) {
+            try {
+                val bytes = Base64.decode(capaDoLivro, Base64.DEFAULT)
+                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                capaLivroSelecionado.setImageBitmap(bitmap)
+            } catch (erro: Exception) {
+                Log.d("BookFast", "Erro ao carregar capa do livro selecionado: $erro")
+                capaLivroSelecionado.setImageResource(R.drawable.bg_book_cover_placeholder)
+            }
+        } else {
+            Log.d("Bookfast", "capa do livro pode estar vazia, ou não existir")
+            capaLivroSelecionado.setImageResource(R.drawable.bg_book_cover_placeholder)
+        }
     }
 }
